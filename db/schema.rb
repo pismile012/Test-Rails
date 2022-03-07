a9b9ca42e84a7770bb54055315c510677c354f3f
nguyentv8 <19.levi.tvn@gmail.com>
2022-03-07 15:02:10 +0700

    Add one-to-many

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_07_064054) do
+ActiveRecord::Schema.define(version: 2022_03_07_073343) do
 
   create_table "action_text_rich_texts", force: :cascade do |t|
     t.string "name", null: false
@@ -55,6 +55,7 @@ ActiveRecord::Schema.define(version: 2022_03_07_064054) do
     t.text "decription"
     t.datetime "created_at", precision: 6
     t.datetime "updated_at", precision: 6
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
