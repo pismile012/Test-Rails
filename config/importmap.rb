7117ee909d25b60a3e7b009e0f976225e96d7685
Van Nguyen <19.levi.tvn@gmail.com>
2022-03-07 12:03:50 +0700

    first commit

---
+++ config/importmap.rb
@@ -0,0 +1,15 @@
+# Pin npm packages by running ./bin/importmap
+
+pin "application", preload: true
+pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
+pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
+pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
+pin_all_from "app/javascript/controllers", under: "controllers"
+# pin "jquery", to: "https://ga.jspm.io/npm:jquery@3.6.0/dist/jquery.js"
+# pin "bootstrap", to: "https://ga.jspm.io/npm:bootstrap@5.1.3/dist/js/bootstrap.esm.js"
+# pin "@popperjs/core", to: "https://ga.jspm.io/npm:@popperjs/core@2.11.2/lib/index.js"
+pin "trix"
+pin "@rails/actiontext", to: "actiontext.js"
+pin "popper", to: 'popper.min.js', preload: true
+pin "bootstrap", to: 'bootstrap.min.js', preload: true
+pin "@rails/ujs", to: "https://ga.jspm.io/npm:@rails/ujs@7.0.2-2/lib/assets/compiled/rails-ujs.js"
