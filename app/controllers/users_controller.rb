44997ae20fe2dd0cead3f2e2f1dd06670291b0c8
nguyentv8 <19.levi.tvn@gmail.com>
2022-03-08 16:21:53 +0700

    Add logout

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,11 @@
 class UsersController < ApplicationController
+    before_action :set_user, only: [:edit, :update, :show]
+    
     def new
         @user = User.new
     end
 
     def show
-        @user = User.find(params[:id])
         @articles = @user.articles.paginate(page: params[:page], per_page: 5)
     end
 
@@ -13,11 +14,10 @@ class UsersController < ApplicationController
     end
 
     def edit
-        @user = User.find(params[:id])
+
     end
 
     def update
-        @user = User.find(params[:id])
         if @user.update(user_params)
             flash[:notice] = "Your account was updated successfully"
             redirect_to @user
@@ -40,5 +40,9 @@ class UsersController < ApplicationController
     private
     def user_params
         params.require(:user).permit(:username, :email, :password)
+    end
+
+    def set_user
+        @user = User.find(params[:id])
     end
 end
